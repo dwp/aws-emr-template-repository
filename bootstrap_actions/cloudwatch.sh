4acba6b4ea5a72f806c9ecb4e75423fc5ae70bf9
levankirvalidze <levan.kirvalidze@dmwgroup.co.uk>
2021-04-07 17:03:53 +0100

    bootstrap actions
    Signed-off-by: levankirvalidze <levan.kirvalidze@dmwgroup.co.uk>

---
+++ bootstrap_actions/cloudwatch.sh
@@ -0,0 +1,83 @@
+#!/bin/bash
+
+set -Eeuo pipefail
+
+cwa_metrics_collection_interval="$1"
+cwa_namespace="$2"
+cwa_log_group_name="$3"
+cwa_bootstrap_loggrp_name="$5"
+cwa_steps_loggrp_name="$6"
+cwa_yarnspark_loggrp_name="$7"
+cwa_tests_loggrp_name="$8"
+
+export AWS_DEFAULT_REGION="$${4}"
+
+# Create config file required for CloudWatch Agent
+mkdir -p /opt/aws/amazon-cloudwatch-agent/etc
+cat > /opt/aws/amazon-cloudwatch-agent/etc/amazon-cloudwatch-agent.json <<CWAGENTCONFIG
+{
+  "agent": {
+    "metrics_collection_interval": $${cwa_metrics_collection_interval},
+    "logfile": "/opt/aws/amazon-cloudwatch-agent/logs/amazon-cloudwatch-agent.log"
+  },
+  "logs": {
+    "logs_collected": {
+      "files": {
+        "collect_list": [
+          {
+            "file_path": "/opt/aws/amazon-cloudwatch-agent/logs/amazon-cloudwatch-agent.log",
+            "log_group_name": "$${cwa_log_group_name}",
+            "log_stream_name": "{instance_id}-amazon-cloudwatch-agent.log",
+            "timezone": "UTC"
+          },
+          {
+            "file_path": "/var/log/messages",
+            "log_group_name": "$${cwa_log_group_name}",
+            "log_stream_name": "{instance_id}-messages",
+            "timezone": "UTC"
+          },
+          {
+            "file_path": "/var/log/secure",
+            "log_group_name": "$${cwa_log_group_name}",
+            "log_stream_name": "{instance_id}-secure",
+            "timezone": "UTC"
+          },
+          {
+            "file_path": "/var/log/cloud-init-output.log",
+            "log_group_name": "$${cwa_log_group_name}",
+            "log_stream_name": "{instance_id}-cloud-init-output.log",
+            "timezone": "UTC"
+          },
+          {
+            "file_path": "/var/log/adg/acm-cert-retriever.log",
+            "log_group_name": "$${cwa_bootstrap_loggrp_name}",
+            "log_stream_name": "{instance_id}-acm-cert-retriever.log",
+            "timezone": "UTC"
+          },
+          {
+            "file_path": "/var/log/adg/emr-setup.log",
+            "log_group_name": "$${cwa_bootstrap_loggrp_name}",
+            "log_stream_name": "{instance_id}-emr-setup.log",
+            "timezone": "UTC"
+          }
+        ]
+      }
+    },
+    "log_stream_name": "$${cwa_namespace}",
+    "force_flush_interval" : 15
+  }
+}
+CWAGENTCONFIG
+
+%{ if emr_release == "5.29.0" ~}
+# Download and install CloudWatch Agent
+curl https://s3.$${AWS_DEFAULT_REGION}.amazonaws.com/amazoncloudwatch-agent-$${AWS_DEFAULT_REGION}/centos/amd64/latest/amazon-cloudwatch-agent.rpm -O
+rpm -U ./amazon-cloudwatch-agent.rpm
+# To maintain CIS compliance
+usermod -s /sbin/nologin cwagent
+
+start amazon-cloudwatch-agent
+%{ else ~}
+sudo /opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-ctl -a fetch-config -m ec2 -c file:/opt/aws/amazon-cloudwatch-agent/etc/amazon-cloudwatch-agent.json
+systemctl start amazon-cloudwatch-agent
+%{ endif ~}
