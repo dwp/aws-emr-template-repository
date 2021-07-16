e7faf537d4a0997f614232a98f45f665e7663316
MattToczek <matthew.toczek@dmwgroup.co.uk>
2021-07-16 09:25:09 +0100

    success with errors rule and slack alert

--- slack-alerts.tf
+++ slack-alerts.tf
@@ -85,6 +85,36 @@ EOF
   }
 }
 
+resource "aws_cloudwatch_event_rule" "aws_emr_template_repository_success_with_errors" {
+  name          = "${local.emr_cluster_name}_succes_with_errors"
+  description   = "checks that all mandatory steps complete but with failures on non mandatory steps"
+  event_pattern = <<EOF
+{
+  "source": [
+    "aws.emr"
+  ],
+  "detail-type": [
+    "EMR Cluster State Change"
+  ],
+  "detail": {
+    "state": [
+      "TERMINATED"
+    ],
+    "name": [
+      "${local.emr_cluster_name}"
+    ],
+    "stateChangeReason": [
+      "{\"code\":\"STEP_FAILURE\",\"message\":\"Steps completed with errors\"}"
+    ]
+  }
+}
+EOF
+
+  tags = {
+    Name = "${local.emr_cluster_name}_success_with_errors"
+  }
+}
+
 resource "aws_cloudwatch_event_rule" "aws_emr_template_repository_running" {
   name          = "${local.emr_cluster_name}_running"
   description   = "checks that aws_emr_template_repository is running"
@@ -178,6 +208,29 @@ resource "aws_cloudwatch_metric_alarm" "aws_emr_template_repository_success" {
     Name              = "${local.emr_cluster_name}_success",
     notification_type = "Information",
     severity          = "Critical"
+  }
+}
+
+resource "aws_cloudwatch_metric_alarm" "aws_emr_template_repository_success_with_errors" {
+  count                     = local.aws_emr_template_repository_alerts[local.environment] == true ? 1 : 0
+  alarm_name                = "${local.emr_cluster_name}_success_with_errors"
+  comparison_operator       = "GreaterThanOrEqualToThreshold"
+  evaluation_periods        = "1"
+  metric_name               = "TriggeredRules"
+  namespace                 = "AWS/Events"
+  period                    = "60"
+  statistic                 = "Sum"
+  threshold                 = "1"
+  alarm_description         = "Monitoring aws_emr_template_repository completion"
+  insufficient_data_actions = []
+  alarm_actions             = [data.terraform_remote_state.security-tools.outputs.sns_topic_london_monitoring.arn]
+  dimensions = {
+    RuleName = aws_cloudwatch_event_rule.aws_emr_template_repository_success_with_errors.name
+  }
+  tags = {
+    Name              = "${local.emr_cluster_name}_success_with_errors",
+    notification_type = "Warning",
+    severity          = "High"
   }
 }
 
