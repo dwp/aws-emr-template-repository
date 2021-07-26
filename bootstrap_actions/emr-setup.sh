8dad9e0c2f537af2ec0ead3c78235c06cf571d21
WW Teo <whay-wang.teo@engineering.digital.dwp.gov.uk>
2021-07-26 12:17:30 +0100

    change underscore directory into dash

--- bootstrap_actions/emr-setup.sh
+++ bootstrap_actions/emr-setup.sh
@@ -76,7 +76,7 @@ EOF
     --truststore-password "$TRUSTSTORE_PASSWORD" \
     --truststore-aliases "${truststore_aliases}" \
     --truststore-certs "${truststore_certs}" \
-    --jks-only true >> /var/log/aws_emr_template_repository/acm-cert-retriever.log 2>&1
+    --jks-only true >> /var/log/aws-emr-template-repository/acm-cert-retriever.log 2>&1
     
     #shellcheck disable=SC2024
     sudo -E acm-cert-retriever \
@@ -84,7 +84,7 @@ EOF
     --acm-key-passphrase "$ACM_KEY_PASSWORD" \
     --private-key-alias "${private_key_alias}" \
     --truststore-aliases "${truststore_aliases}" \
-    --truststore-certs "${truststore_certs}"  >> /var/log/aws_emr_template_repository/acm-cert-retriever.log 2>&1 # No sudo needed to write to file, so redirect is fine
+    --truststore-certs "${truststore_certs}"  >> /var/log/aws-emr-template-repository/acm-cert-retriever.log 2>&1 # No sudo needed to write to file, so redirect is fine
     
     cd /etc/pki/ca-trust/source/anchors/ || exit
 
@@ -119,4 +119,4 @@ EOF
     
     log_wrapper_message "Completed the emr-setup.sh step of the EMR Cluster"
 
-) >> /var/log/aws_emr_template_repository/emr-setup.log 2>&1
+) >> /var/log/aws-emr-template-repository/emr-setup.log 2>&1
