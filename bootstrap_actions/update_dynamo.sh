d32f0866a99acc088b80ea8e29b48c3af33efe7b
levankirvalidze <levankirvalidze@digital.uc.dwp.gov.uk>
2021-08-31 13:37:07 +0100

    use latest update dynamo script
    Signed-off-by: levankirvalidze <levankirvalidze@digital.uc.dwp.gov.uk>

--- bootstrap_actions/update_dynamo.sh
+++ bootstrap_actions/update_dynamo.sh
@@ -17,10 +17,11 @@
 
   STEP_DETAILS_DIR=/mnt/var/lib/info/steps
   CORRELATION_ID_FILE=/opt/emr/correlation_id.txt
+  S3_PREFIX_FILE=/opt/emr/s3_prefix.txt
   SNAPSHOT_TYPE_FILE=/opt/emr/snapshot_type.txt
   OUTPUT_LOCATION_FILE=/opt/emr/output_location.txt
   EXPORT_DATE_FILE=/opt/emr/export_date.txt
-  
+
   DATE=$(date '+%Y-%m-%d')
   CLUSTER_ID=$(jq '.jobFlowId' < /mnt/var/lib/info/job-flow.json)
   CLUSTER_ID="$${CLUSTER_ID//\"}"
@@ -32,12 +33,13 @@
 
   FINAL_STEP_NAME="${dynamodb_final_step}"
 
-  while [[ ! -f "$CORRELATION_ID_FILE" ]] && [[ ! -f "$SNAPSHOT_TYPE_FILE" ]] && [[ ! -f "$EXPORT_DATE_FILE" ]]
+  while [[ ! -f "$CORRELATION_ID_FILE" ]] && [[ ! -f "$S3_PREFIX_FILE" ]] && [[ ! -f "$SNAPSHOT_TYPE_FILE" ]] && [[ ! -f "$EXPORT_DATE_FILE" ]]
   do
     sleep 5
   done
 
   CORRELATION_ID=$(cat $CORRELATION_ID_FILE)
+  S3_PREFIX=$(cat $S3_PREFIX_FILE)
   SNAPSHOT_TYPE=$(cat $SNAPSHOT_TYPE_FILE)
   EXPORT_DATE=$(cat $EXPORT_DATE_FILE)
   DATA_PRODUCT="AWS_EMR_TEMPLATE_REPOSITORY"
@@ -62,6 +64,7 @@
     echo "$OUTPUT_LOCATION"
   }
 
+  MAX_RETRY=10
   processed_files=()
   dynamo_update_item() {
     current_step="$1"
@@ -71,10 +74,10 @@
     ttl_value=$(get_ttl)
     output_location_value=$(get_output_location)
 
-    log_wrapper_message "Updating DynamoDB with Correlation_Id: $CORRELATION_ID, DataProduct: $DATA_PRODUCT, Date: $EXPORT_DATE, Cluster_Id: $CLUSTER_ID, S3_Prefix_Analytical_DataSet: $output_location_value, Snapshot_Type: $SNAPSHOT_TYPE, TimeToExist: $ttl_value, CurrentStep: $current_step, Status: $status, Run_Id: $run_id"
+    log_wrapper_message "Updating DynamoDB with Correlation_Id: $CORRELATION_ID, DataProduct: $DATA_PRODUCT, Date: $EXPORT_DATE, Cluster_Id: $CLUSTER_ID, S3_Prefix_Snapshots: $S3_PREFIX, S3_Prefix_Analytical_DataSet: $output_location_value, Snapshot_Type: $SNAPSHOT_TYPE, TimeToExist: $ttl_value, CurrentStep: $current_step, Status: $status, Run_Id: $run_id"
 
-    update_expression="SET #d = :s, Cluster_Id = :v, Snapshot_Type = :x, TimeToExist = :z"
-    expression_values="\":s\": {\"S\":\"$EXPORT_DATE\"}, \":v\": {\"S\":\"$CLUSTER_ID\"}, \":x\": {\"S\":\"$SNAPSHOT_TYPE\"}, \":z\": {\"N\":\"$ttl_value\"}"
+    update_expression="SET #d = :s, Cluster_Id = :v, S3_Prefix_Snapshots = :w, Snapshot_Type = :x, TimeToExist = :z"
+    expression_values="\":s\": {\"S\":\"$EXPORT_DATE\"}, \":v\": {\"S\":\"$CLUSTER_ID\"}, \":w\": {\"S\":\"$S3_PREFIX\"}, \":x\": {\"S\":\"$SNAPSHOT_TYPE\"}, \":z\": {\"N\":\"$ttl_value\"}"
     expression_names="\"#d\":\"Date\""
 
     if [[ -n "$current_step" ]] && [[ "$current_step" != "NOT_SET" ]]; then
@@ -113,6 +116,7 @@
       if [[ "$${processed_files[@]}" =~ "$${i}" ]]; then # We do not want a REGEX check here so it is ok
         continue
       fi
+      RETRY_COUNT=0
       state=$(jq -r '.state' "$i")
       while [[ "$state" != "$COMPLETED_STATUS" ]]; do
         step_script_name=$(jq -r '.args[0]' "$i")
@@ -121,22 +125,33 @@
         fi
         CURRENT_STEP=$(echo "$step_script_name" | sed 's:.*/::' | cut -f 1 -d '.')
         state=$(jq -r '.state' "$i")
-        if [[ "$state" == "$FAILED_STATUS" ]] || [[ "$state" == "$CANCELLED_STATUS" ]]; then
-          log_wrapper_message "Failed step. Step Name: $CURRENT_STEP, Step status: $state"
-          dynamo_update_item "$CURRENT_STEP" "$FAILED_STATUS" "NOT_SET"
-          exit 0
-        fi
-        if [[ "$CURRENT_STEP" == "$FINAL_STEP_NAME" ]] && [[ "$state" == "$COMPLETED_STATUS" ]]; then
-          dynamo_update_item "$CURRENT_STEP" "$COMPLETED_STATUS" "NOT_SET"
-          log_wrapper_message "All steps completed. Final step Name: $CURRENT_STEP, Step status: $state"
-          exit 0
-        fi
-        if [[ "$PREVIOUS_STATE" != "$state" ]] && [[ "$PREVIOUS_STEP" != "$CURRENT_STEP" ]]; then
-          dynamo_update_item "$CURRENT_STEP" "NOT_SET" "NOT_SET"
-          log_wrapper_message "Successful step. Last step name: $PREVIOUS_STEP, Last step status: $PREVIOUS_STATE, Current step name: $CURRENT_STEP, Current step status: $state"
-          processed_files+=( "$i" )
+        if [[ -n "$state" ]] && [[ -n "$CURRENT_STEP" ]]; then
+          if [[ "$state" == "$FAILED_STATUS" ]] || [[ "$state" == "$CANCELLED_STATUS" ]]; then
+            log_wrapper_message "Failed step. Step Name: $CURRENT_STEP, Step status: $state"
+            dynamo_update_item "$CURRENT_STEP" "$FAILED_STATUS" "NOT_SET"
+            exit 0
+          fi
+          if [[ "$CURRENT_STEP" == "$FINAL_STEP_NAME" ]] && [[ "$state" == "$COMPLETED_STATUS" ]]; then
+            dynamo_update_item "$CURRENT_STEP" "$COMPLETED_STATUS" "NOT_SET"
+            log_wrapper_message "All steps completed. Final step Name: $CURRENT_STEP, Step status: $state"
+            exit 0
+          fi
+          if [[ "$PREVIOUS_STATE" != "$state" ]] && [[ "$PREVIOUS_STEP" != "$CURRENT_STEP" ]]; then
+            dynamo_update_item "$CURRENT_STEP" "NOT_SET" "NOT_SET"
+            log_wrapper_message "Successful step. Last step name: $PREVIOUS_STEP, Last step status: $PREVIOUS_STATE, Current step name: $CURRENT_STEP, Current step status: $state"
+            processed_files+=( "$i" )
+          else
+            sleep 0.2
+          fi
         else
-          sleep 0.2
+          if [[ "$RETRY_COUNT" -ge "$MAX_RETRY" ]]; then
+            log_wrapper_message "Could not parse one or more json attributes from $i. Last Step Name: $PREVIOUS_STEP. Last State Name: $PREVIOUS_STATE."
+            dynamo_update_item "$CURRENT_STEP" "$FAILED_STATUS" "NOT_SET"
+            exit 1
+          fi
+          RETRY_COUNT=$((RETRY_COUNT+1))
+          log_wrapper_message "Sleeping... Failed reading step file $RETRY_COUNT times. Could not parse one or more json attributes from $i. Last Step Name: $PREVIOUS_STEP. Last State Name: $PREVIOUS_STATE."
+          sleep 1
         fi
         PREVIOUS_STATE="$state"
         PREVIOUS_STEP="$CURRENT_STEP"
@@ -157,7 +172,7 @@
       log_wrapper_message "Previous failed status found, creating step_to_start_from.txt"
       CURRENT_STEP=$(echo "$response" | jq -r .'Item.CurrentStep.S')
       echo "$CURRENT_STEP" >> /opt/emr/step_to_start_from.txt
-    fi   
+    fi
 
     CURRENT_RUN_ID=$(echo "$response" | jq -r .'Item.Run_Id.N')
     NEW_RUN_ID=$((CURRENT_RUN_ID+1))
